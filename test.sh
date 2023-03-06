0dc2be2c59643b6332fbfef88f28b13b950a90e3
Henrique de Lima Florindo <catskillet.hflorindo@hubfintech.com.br>
2023-03-06 15:23:08 -0300

    Creating a folder for the listings and automating the test script

--- test.sh
+++ test.sh
@@ -1,21 +1,38 @@
-nasm listing_0037_single_register_mov.asm
-nasm listing_0038_many_register_mov.asm
-nasm listing_0039_more_movs.asm
-nasm listing_0040_challenge_movs.asm
+mkdir -p binary_out
+cd listings
+
+for i in *.asm; do
+	asm_file="$i"
+	out=${asm_file%????}
+    nasm "$i" -o "../binary_out/$out.out"
+done
+
+cd ..
 
 clang main.c -o simulator.out
 
-./simulator.out listing_0037_single_register_mov > 37.asm
-./simulator.out listing_0038_many_register_mov > 38.asm
-./simulator.out listing_0039_more_movs > 39.asm
-./simulator.out listing_0040_challenge_movs > 40.asm
+mkdir -p output
+cd binary_out
 
-nasm 37.asm
-nasm 38.asm
-nasm 39.asm
-nasm 40.asm
+for i in *.out; do
+	out_file="$i"
+	out=${out_file%????}
+	../simulator.out "$i" > "../output/$out.asm"
+done
 
-diff 37 listing_0037_single_register_mov
-diff 38 listing_0038_many_register_mov
-diff 39 listing_0039_more_movs
-diff 40 listing_0040_challenge_movs
+cd ..
+cd output
+
+for i in *.asm; do
+	asm_file="$i"
+	out=${asm_file%????}
+    nasm "$i" -o "../binary_out/generated_$out"
+done
+
+cd ..
+cd binary_out
+for i in *.out; do
+	out="$i"
+	gen="generated_${out%????}"
+	diff "$i" "$gen"
+done
